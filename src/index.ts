7dfcb4384b9b4595c3e15f5a0b14b1418716e8fc
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2022-06-21 17:37:32 +0200

    chore: specify engine

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import fs from 'fs'
 
 import vercelConfig from './vercel.json'
 
+console.log('Creating Vercel config file...')
 // write the config to vercel.json
 fs.writeFileSync('./vercel.json', JSON.stringify(vercelConfig, null, 2))
